9e0157d351fc50cfff0e5fcead826681f83c5ad1
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-09-27 02:02:24 +0600

    Скорректированы модели, провдены соответствующие миграции, обновлена фикстура

--- app/webapp/models.py
+++ app/webapp/models.py
@@ -2,23 +2,27 @@ from django.db import models
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=200, null=False, blank=False, verbose_name='Категория')
-    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
-    changed_at = models.DateField(auto_now=True, verbose_name='Дата обновления')
+    category = models.CharField(max_length=50, null=False, blank=False, verbose_name='Категория')
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    changed_at = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Дата обновления')
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=250, null=False, blank=False, verbose_name='Продукт')
+    no_category = (None, '')
+
+    title = models.CharField(max_length=65, null=False, blank=False, verbose_name='Продукт')
     price = models.DecimalField(null=False, blank=False, max_digits=12, decimal_places=2, verbose_name='Цена')
+    image = models.CharField(max_length=300, null=True, blank=True, verbose_name="Изображние")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
     category = models.ForeignKey(
         verbose_name='Категория',
         to='webapp.Category',
         null=True,
         blank=True,
-        related_name='categories',
+        related_name='products',
         on_delete=models.RESTRICT
     )
-    image = models.CharField(max_length=300, null=True, blank=True, verbose_name="Описание")
-    description = models.TextField(null=False, blank=False, verbose_name="Описание")
-    date_created = models.DateField(auto_now_add=True, verbose_name='Дата создания')
-    date_updated = models.DateField(auto_now=True, null=True, blank=True, verbose_name='Дата обновления')
+    created_at = models.DateTimeField(auto_now_add=True, null=False, verbose_name='Дата создания')
+    changed_at = models.DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True,
+                                      verbose_name='Дата обновления')
